85696a9d14bfaff062a71bd3848fc73315388cb5
197491 <197491@TCR-00349>
2022-02-20 17:23:05 -0300

    Script para rodar banco de dados

---
+++ ScriptSQL.sql
@@ -0,0 +1,9 @@
+﻿--Rodar esse Scripts para gerar banco de dados e tabelas
+
+CREATE DATABASE todolis;
+
+CREATE TABLE tarefas(
+    id integer AUTO_INCREMENT,
+    nome varchar(200) not null,
+    PRIMARY KEY(id)
+);
